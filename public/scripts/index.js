0365f7509d30e93fddf82694aae036795291d56a
Seb <sebastien.e.wong@gmail.com>
2021-01-29 13:23:28 -0700

    Updated portfolio, made visual summary for p0, cleanup in p0 script

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -4,3 +4,11 @@ const URL = location.origin; // root domain
 $("#go-p0").on("click", e => {
     window.location.assign(`${URL}/pages/p0.html`);
 });
+
+$("#src-p0").on("click", e => {
+    window.open('https://github.com/sebastienwong/sebwong-portfolio');
+})
+
+$("#det-p0").on("click", e => {
+    window.location.assign(`${URL}/p0_visual_summary/index.html`);
+});
