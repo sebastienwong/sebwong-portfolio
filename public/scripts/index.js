a0399aa78150af9fff96e7a94ad1c81b7a0eabe6
Seb <sebastien.e.wong@gmail.com>
2021-03-02 21:37:58 -0700

    Rerouted

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,7 +6,7 @@ $("#go-p0").on("click", e => {
 });
 
 $("#src-p0").on("click", e => {
-    window.open('https://github.com/sebastienwong/sebwong-portfolio');
+    window.open('https://github.com/sebastienwong/sebwong-portfolio/tree/master/public/p0');
 })
 
 $("#det-p0").on("click", e => {
@@ -16,4 +16,12 @@ $("#det-p0").on("click", e => {
 
 $("#go-p1").on("click", e => {
     window.location.assign(`${URL}/p1/pages/p1.html`);
+});
+
+$("#src-p1").on("click", e => {
+    window.open('https://github.com/sebastienwong/sebwong-portfolio/tree/master/public/p1');
+})
+
+$("#det-p1").on("click", e => {
+    window.location.assign(`${URL}/p1/p0_visual_summary/index.html`);
 });
