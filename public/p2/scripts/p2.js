8e06987505d6a253587112ca1233c74d05a98342
Sebastien Wong <sebastien.e.wong@gmail.com>
2021-03-20 15:37:56 -0600

    Setup webrtc following tutorials

--- public/p2/scripts/p2.js
+++ public/p2/scripts/p2.js
@@ -1,21 +1,172 @@
-const socket = io();
+// Adapted from https://github.com/shanet/WebRTC-Example
 
-const videoHolder = document.getElementById("video-holder")
+var localVid = $("#localVideo");
+var remoteVid = $("#remoteVideo");
+var btn1 = $("#btn-client1");
+var btn2 = $("#btn-client2");
+var theInput = $("#msgInput");
+var callBtn = $("#btn-call");
+var msgDiv = $("#msgDiv");
 
-const myVideo = document.createElement("video");
-myVideo.muted = true;
+var localStream;
+var peerConnection;
+var serverConnection;
 
-navigator.mediaDevices.getUserMedia({
-    video: true,
-    audio: true
-}).then(stream => {
-    addVideoStream(myVideo, stream)
+const peerConnectionConfig = {
+    iceServers: [
+        { urls: "stun:stun.stunprotocol.org:3478" },
+        { urls: "stun:stun.l.google.com:19302" },
+        { urls: "stun:stun1.l.google.com:19302" },
+        { urls: "stun:stun2.l.google.com:19302" },
+        { urls: "stun:stun3.l.google.com:19302" },
+        { urls: "stun:stun4.l.google.com:19302" },
+        { urls: "stun:stun.ekiga.net" },
+        { urls: "stun:stun.fwdnet.net" },
+        { urls: "stun:stun.ideasip.com" },
+        { urls: "stun:stun.iptel.org" },
+    ],
+};
+
+const MessageType = {
+    SERVER_INFO: 0,
+    CLIENT1: 1,
+    CLIENT2: 2,
+    CALL_REQUEST: 3,
+};
+
+btn1.on("click", () => {
+    getWebcam();
+    btn2.prop("disabled", true);
+    destination = "wss://" + location.host + "/client1";
+    serverConnection = new WebSocket(destination);
+    serverConnection.onmessage = handleMessage;
 });
 
-function addVideoStream(video, stream) {
-    video.srcObject = stream;
-    video.addEventListener('loadedmetadata', () => {
-        video.play();
-    });
-    videoHolder.append(video);
+btn2.on("click", () => {
+    getWebcam();
+    btn1.prop("disabled", true);
+    destination = "wss://" + location.host + "/client2";
+    serverConnection = new WebSocket(destination);
+    serverConnection.onmessage = handleMessage;
+});
+
+callBtn.on("click", () => {
+    start(true);
+});
+
+function getWebcam() {
+    if (navigator.getUserMedia) {
+        navigator.getUserMedia({
+                video: true,
+                audio: true,
+            },
+            (stream) => {
+                // success
+                localStream = stream;
+                localVid.prop("srcObject", stream);
+            },
+            (error) => {
+                // error
+                console.error(error);
+            }
+        );
+    } else {
+        alert("Your browser does not support getUserMedia API");
+    }
+}
+
+function start(isCaller) {
+    peerConnection = new RTCPeerConnection(peerConnectionConfig);
+    peerConnection.onicecandidate = gotIceCandidate;
+    peerConnection.ontrack = gotRemoteStream;
+    peerConnection.addStream(localStream);
+
+    if (isCaller) {
+        peerConnection.createOffer().then(createdDescription).catch(errorHandler); // using chained Promises for async
+    }
+}
+
+function gotIceCandidate(event) {
+    if (event.candidate != null) {
+        serverConnection.send(
+            JSON.stringify({
+                type: MessageType.CALL_REQUEST,
+                ice: event.candidate,
+                message: "Sending ICE candidate",
+            })
+        );
+    }
+}
+
+function createdDescription(description) {
+    console.log("got description");
+
+    peerConnection
+        .setLocalDescription(description)
+        .then(() => {
+            serverConnection.send(
+                JSON.stringify({
+                    type: MessageType.CALL_REQUEST,
+                    sdp: peerConnection.localDescription,
+                    message: "Requesting call",
+                })
+            );
+        })
+        .catch(errorHandler);
+}
+
+function gotRemoteStream(event) {
+    console.log("got remote stream");
+    remoteVid.prop("srcObject", event.streams[0]);
+    msgDiv.html("Connected to peer.");
+}
+
+function handleMessage(mEvent) {
+    var msg = JSON.parse(mEvent.data);
+
+    switch (msg.type) {
+        case MessageType.SERVER_INFO:
+            msgDiv.html(msg.message);
+            break;
+
+            // Message came from Client 1, Handle as Client2
+        case MessageType.CLIENT1:
+            break;
+
+            // Message came from Client 2, Handle as Client1
+        case MessageType.CLIENT2:
+            break;
+
+        case MessageType.CALL_REQUEST:
+            if (!peerConnection) {
+                msgDiv.html("Receiving Call!");
+                start(false);
+            }
+
+            // Are we on the SDP stage or the ICE stage of the handshake?
+            if (msg.sdp) {
+                peerConnection
+                    .setRemoteDescription(new RTCSessionDescription(msg.sdp))
+                    .then(() => {
+                        // Only create answers in response to offers
+                        if (msg.sdp.type == "offer") {
+                            peerConnection
+                                .createAnswer()
+                                .then(createdDescription)
+                                .catch(errorHandler);
+                        }
+                    })
+                    .catch(errorHandler);
+            } else if (msg.ice) {
+                peerConnection
+                    .addIceCandidate(new RTCIceCandidate(msg.ice))
+                    .catch(errorHandler);
+            }
+        default:
+            break;
+    }
+}
+
+function errorHandler(error) {
+    console.error(error);
 }
--- web.js
+++ web.js
@@ -1,18 +1,156 @@
-var path = require('path');
-var express = require('express');
+const path = require("path");
+const express = require("express");
+const ws = require("ws");
+const { sign } = require("crypto");
 const PORT = process.env.PORT || 5000;
+const DOMAIN = "https://sebwong-portfolio.herokuapp.com/";
 
-const app = express()
-  .use(express.static(path.join((__dirname, 'public'))))
-  .get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/pages/index.html'));
-  })
+var app = express();
 
-  .listen(PORT);
+const MessageType = {
+    SERVER_INFO: 0,
+    CLIENT1: 1,
+    CLIENT2: 2,
+    CALL_REQUEST: 3
+};
 
-//const http = require('http').Server(app);
-const io = require('socket.io')(app);
+var wsServer = new ws.Server({ noServer: true });
+app
+    .use(express.static(path.join(__dirname, "public")))
+    .use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", DOMAIN);
+        res.header(
+            "Access-Control-Allow-Headers",
+            "Origin, X-Requested-With, Content-Type, Accept"
+        );
+        next();
+    })
+    .get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "public/pages/index.html"));
+    })
+    .get("/model", (req, res) => {
+        res.sendFile(path.join(__dirname, "public/pages/index.html"));
+    })
+    .listen(PORT)
+    .on("upgrade", (request, socket, head) => {
+        wsServer.handleUpgrade(request, socket, head, (socket) => {
+            wsServer.emit("connection", socket, request);
+        });
+    });
 
-io.on("connection", (socket) => {
-  console.log("someone is here");
+var clients = {}; // iterating over object key:value pairs => object[key] returns key's value
+
+wsServer.on("connection", (socket, request) => {
+    // route to endpoint handlers
+    console.log("caught a connection");
+    switch (request.url) {
+        case "/client1":
+            if (!clients.client1) {
+                console.log("Client 1 Connected");
+                clients.client1 = socket;
+                socket.send(
+                    JSON.stringify({
+                        type: MessageType.SERVER_INFO,
+                        message: "Connected to server. Waiting for peer..."
+                    })
+                );
+            } else {
+                socket.close(1013, "Client 1 already taken. Try again later.");
+            }
+            break;
+
+        case "/client2":
+            if (!clients.client2) {
+                console.log("Client 2 Connected");
+                clients.client2 = socket;
+                socket.send(
+                    JSON.stringify({
+                        type: MessageType.SERVER_INFO,
+                        message: "Connected to server. Waiting for peer..."
+                    })
+                );
+            } else {
+                socket.close(1013, "Client 2 already taken. Try again later.");
+            }
+            break;
+
+        default:
+            console.log("default");
+            socket.close(1000, "Endpoint Not Found");
+            break;
+    }
+
+    // handle and route messages to endpoint handlers
+    socket.onmessage = (mEvent) => {
+        var msg = JSON.parse(mEvent.data);
+        switch (request.url) {
+            case "/client1": //tut5exB
+                if (clients.client2 != undefined) { // clients2 exists, forward the message
+                    console.log("Forwarded message from Client 1 to Client 2");
+                    console.log(msg);
+                    clients.client2.send(JSON.stringify(msg));
+                } else {
+                    clients.client1.send(
+                        JSON.stringify({
+                            type: MessageType.SERVER_INFO,
+                            message: "Waiting for Client2 to connect..."
+                        })
+                    );
+                }
+                break;
+
+            case "/client2": //tut5exc
+                if (clients.client1 != undefined) {
+                    console.log("Forwarded message from Client 2 to Client 1");
+                    console.log(msg);
+                    clients.client1.send(JSON.stringify(msg)); // clients1 exists, forward the message
+                } else {
+                    clients.client2.send(
+                        JSON.stringify({
+                            type: MessageType.SERVER_INFO,
+                            message: "Waiting for Client1 to connect..."
+                        })
+                    );
+                }
+                break;
+
+            default:
+                socket.close(1000, "Endpoint Not Found");
+                break;
+        }
+    };
+    socket.onclose = (e) => {
+        console.log("Socket closed: " + e.code + " " + e.reason); // debug message to confirm closed socket.
+        clearInterval(interval); // stop heartbeat for this socket
+
+        if (e.code == 1001) {
+            // code 1001: client closed socket, disconnect all clients and delete them
+            for (var s in clients) {
+                clients[s].close(4000, "Peer disconnected");
+                clients[s] = undefined;
+            }
+        }
+    };
+
+    // establish ping-pong heartbeats
+    socket.isAlive = true;
+    socket.on("pong", () => {
+        // ping-pong heartbeat
+        console.log("pong at " + request.url);
+        socket.isAlive = true; // a successful ping-pong means connection is still alive
+    });
+
+    var interval = setInterval(() => {
+        if (socket.isAlive === false) {
+            // didn't get a pong back within 10s
+            socket.terminate(); // kill the socket in cold blood
+            clients[
+                request.url.slice(1) /* remove the / from the endpoint name */
+            ] = undefined;
+            return;
+        }
+
+        socket.isAlive = false; // first assume connection is dead
+        socket.ping(); // do the "heartbeat" to 'revive' it
+    }, 10000); // 10s
 });
